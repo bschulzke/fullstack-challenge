cbb76dd887bb9adcc56394b9b0b6fcf73ee02389
Ben Schulzke <benschulzke@gmail.com>
2025-04-20 17:31:21 -0700

    make id column in organizations table more specific

--- server/db.ts
+++ server/db.ts
@@ -5,7 +5,7 @@ function initializeDatabase() {
   db.prepare(
     `
     CREATE TABLE IF NOT EXISTS organizations (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      organization_id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
       updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
